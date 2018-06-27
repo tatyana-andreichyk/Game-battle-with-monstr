22d0b106095e69a482668b682a8af204fcf750d0
tatyana-andreichyk <tatyana.andreichyk@gmail.com>
2018-06-27 12:21:29 +0300

    add password field

--- login.js
+++ login.js
@@ -4,13 +4,14 @@ let btnLogin = document.getElementById('button_login');
 function goToGamePage() {
 
     let userName = document.getElementById('user_name').value;
+    let userPassw = document.getElementById('user_password').value;
         
-    if (userName !== '') {
+    if (userName !== '' && userPassw !== '') {
         localStorage.setItem('name', userName);
         
         window.location.assign('game_page.html');
     } else {   
-        alert('Your name field cannot be empty!')
+        alert('Your name field and password field cannot be empty!')
     }  
 } 
 
